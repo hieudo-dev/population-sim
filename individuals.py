947243d930d45e62de24dfb9bcdce2fb558eabd2
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-03-22 02:47:57 -0400

    Implement model's relations and processes

--- individuals.py
+++ individuals.py
@@ -1,7 +1,62 @@
 from person import Person
+from randoms import Uniform
 
 class Male(Person):
-	pass
+	def FindCouple(self, population):
+		for girl in population:
+			if girl is Female and self.Like(girl):
+				self.GoOutWith(girl)
+	
 
 class Female(Person):
-	pass
+	isPregnant = False
+	dueDate = -1
+
+	def TryForBaby(self, currentTime):
+
+		if not self.IsSingle() and \
+				self.childCount < min(self.DesiredChildrenCount(), self.couple.DesiredChildrenCount()):
+			# Let's try for a baby :)
+			prob = 0
+			if self.age < 15:
+				prob = .2
+			elif self.age < 21:
+				prob = .45
+			elif self.age < 35:
+				prob = .8
+			elif self.age < 45:
+				prob = .4
+			elif self.age < 60:
+				prob = .2
+			elif self.age <= 125:
+				prob = .05
+			
+			if Uniform() <= prob:
+				self.isPregnant = True
+				self.dueDate = currentTime + 1
+				return True
+		return False
+
+	def GiveBirth(self):
+		childCount = 0
+		rv = Uniform()
+		if rv < .7:
+			childCount = 1
+		elif rv < .86:
+			childCount = 2
+		elif rv < .94:
+			childCount = 3
+		elif rv < .98:
+			childCount = 4
+		else:
+			childCount = 5
+
+		childs = []
+		for i in range(childCount):
+			if Uniform() < .5:
+				childs.append(Male())
+			else:
+				childs.append(Female())
+
+		self.isPregnant = False
+		return childs
--- person.py
+++ person.py
@@ -1,38 +1,110 @@
+from randoms import Exponential, Uniform
+from math import ceil, floor
+
 class Person:
 	age = 0
 	couple = None
 	isDead = False
+	isSad = False
 	childCount = 0
-	coupleReadyTime = -1			# Simulator's time at which this person will be available
 
 	def __init__(self, age=0):
 		self.age = age
 
-	def isSingle(self):
+	def IsSingle(self):
 		return self.couple == None
-
-	
-
-
-	def FindCouple(self, population):
-		pass
 
 
 	def WantsCouple(self):
-		pass
+		prob = 0
+		if 12 <= self.age and self.age < 15:
+			prob = .6
+		elif self.age < 21:
+			prob = .65
+		elif self.age < 35:
+			prob = .8
+		elif self.age < 45:
+			prob = .6
+		elif self.age < 60:
+			prob = .5
+		elif self.age < 125:
+			prob = .2
+			
+		return Uniform() <= prob
 
-	def SuitableCouple(self, individual):
-		pass
+	def Like(self, individual):
+		return not self.isSad and self.age >= 12 and self.IsSingle() and self.WantsCouple() and \
+			not individual.isSad and individual.age >= 12 and	individual.IsSingle() and individual.WantsCouple()
 
-	def MakeCouple(self, individual):
-		pass
+	def GoOutWith(self, individual):
+		ageDif = abs(self.age - individual.age)
+		prob = 0
+		if ageDif < 5:
+			prob = .45
+		elif ageDif < 10:
+			prob = .4
+		elif ageDif < 15:
+			prob = .35
+		elif ageDif < 20:
+			prob = .25
+		else:
+			prob = .15
+		
+		if Uniform() <= prob:
+			# They are officially a couple
+			self.couple = individual
+			individual.couple = self
+			return True
+			
+		return False
+
+	def HaveRelationshipCrisis(self):
+		# The crisis can make the couple break up
+		if Uniform() <= .2:
+			# Break up :(
+			self.BreakUpCouple()
+			
+	def HeartbreakDuration(self):
+		lmda = 0
+		if 12 < self.age and self.age < 15:
+			lmda =  3
+		elif self.age < 21:
+			lmda = 6
+		elif self.age < 35:
+			lmda = 9
+		elif self.age < 45:
+			lmda = 12
+		elif self.age < 60:
+			lmda = 24
+		elif self.age < 125:
+			lmda = 48
+			
+		rv = Exponential(1 / lmda)
+		return ceil(rv) if rv - floor(rv) >= .5 else floor(rv)
 
 	def BreakUpCouple(self):
-		pass
-	
+		if not self.IsSingle():
+			self.couple.couple = None
+			self.couple = None
+		
 
 	def WantsMoreChildren(self):
-		pass
+		return self.childCount < self.DesiredChildrenCount()
 
 	def DesiredChildrenCount(self):
-		pass
+		# Amount of children:
+		# 1: .20 
+		# 2: .55 
+		# 3: .15
+		# 4: .8 
+		# 5: .2
+		rv = Uniform()
+		if rv <= .2:
+			return 1
+		elif rv <= .75:
+			return 2
+		elif rv <= .90:
+			return 3
+		elif rv <= 98:
+			return 4
+		return 5
