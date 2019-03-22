ca28bd0895bc9100c32955b27d98c2eb55c069db
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-03-22 13:56:04 -0400

    Implement Simulator

---
+++ src/simulator.py
@@ -0,0 +1,105 @@
+from individuals import Female, Male, Person
+from math import floor
+from events import BirthDayEvent, GiveBirthEvent
+import heapq
+import numpy as nmp
+
+
+def IsDying(person):
+	prob = 0
+	if person.age < 12:
+		prob = .25
+	elif person.age < 45:
+		prob = .1 if person is Male else .15
+	elif person.age < 76:
+		prob = .3 if person is Male else .35
+	else:
+		prob = .7 if person is Male else .65
+	return nmp.random.uniform() <= prob
+
+def printpop(pop):
+	for i in pop:
+		printper(i)
+
+def printper(i):
+	if type(i) is Female:
+		print("F ", i.age, i.IsSingle())
+	else:
+		print("M ", i.age, i.IsSingle())
+
+
+class Simulator:
+	population = []
+	events = []
+	eventKey = 0
+	time = 0
+	maxTime = 0
+	births = 0
+	peakCount = 0
+
+	def __init__(self, M, F, maxTime):
+		self.GeneratePopulation(M, F)
+		self.maxTime = maxTime
+		
+	def GeneratePopulation(self, M, F):
+		self.population = []
+
+		for i in range(M):
+			rv = nmp.random.uniform(12, 30)
+			p = Male(floor(rv))
+			self.population.append(p)
+			self.AddEvent(p, BirthDayEvent)
+
+		for i in range(F):
+			rv = nmp.random.uniform(12, 30)
+			p = Female(floor(rv))
+			self.population.append(p)
+			self.AddEvent(p, BirthDayEvent)
+			
+
+	def AddEvent(self, person, event, time=-1):
+		time = self.time if time == -1 else time
+		self.eventKey += 1
+		heapq.heappush(self.events, (time, self.eventKey, person, event(person)))
+
+
+	def Execute(self):
+		printpop(self.population)
+		while self.events != []:
+			time, secondarykey, person, event = heapq.heappop(self.events)
+			self.time = time
+			self.peakCount = max(self.peakCount, self.population.__len__())
+			if time > self.maxTime:
+				self.time = self.maxTime
+				break
+
+			if person.isDead:
+				continue
+
+			event(self)
+			
+			if type(person) is Male and person.age >= 12:
+				person.FindCouple(self.population)
+
+			if type(person) is Male and not person.IsSingle():
+				person.HaveRelationshipCrisis()
+
+			if type(person) is Female and not person.isPregnant:
+				if person.TryForBaby(self.time):
+					self.AddEvent(person, GiveBirthEvent, self.time + 9)
+
+
+			if IsDying(person):
+				person.BreakUpCouple()
+				person.isDead = True
+				self.population.remove(person)
+				# print("X Died at age ", person.age)
+
+		# printpop(self.population)
+		print("Population: ", self.population.__len__())
+		print("Births: ", self.births)
+		print("Peak Population: ", self.peakCount)
+		print("Time: ", self.time)
+
+s = Simulator(10,10, 600)
+s.Execute()
