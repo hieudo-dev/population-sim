b4dcb56b752bbead420ba01cda355b84f71613be
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-03-24 11:43:52 -0400

    Add parameter to Uniform RV generator

--- src/randoms.py
+++ src/randoms.py
@@ -5,5 +5,5 @@ from math import log
 def Exponential(lmda):
 	return -1 * (1.0 / lmda) * log(Uniform())
 
-def Uniform():
-	return np.random.uniform()
+def Uniform(l=0.0, r=1.0):
+	return np.random.uniform(low=l, high=r)
